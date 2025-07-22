155acd3665fd7f218435757972ceda406cf747b6
Brian Alvarez <balvarez@astranis.com>
2025-07-22 16:07:04 -0700

    fix fn key names

--- firmware/QMK/VIAL_source/totem/keymaps/alvarezb/keymap.c
+++ firmware/QMK/VIAL_source/totem/keymaps/alvarezb/keymap.c
@@ -134,9 +134,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                     └─────────┴─────────┴─────────┘└─────────┴─────────┴─────────┘ */
 
     [_ADJUST] = LAYOUT(
-                QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, KC_F7, KC_F8, KC_F9, KC_12,
-                DB_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, KC_F4, KC_F5, KC_F6, KC_11,
-        MAKE_H, CG_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, KC_F1, KC_F2, KC_F3, KC_10, XXXXXXX,
+                QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, KC_F7, KC_F8, KC_F9, KC_F12,
+                DB_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, KC_F4, KC_F5, KC_F6, KC_F11,
+        MAKE_H, CG_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, KC_F1, KC_F2, KC_F3, KC_F10, XXXXXXX,
                                   _______, _______, _______,    _______, _______, _______
     )
 };
